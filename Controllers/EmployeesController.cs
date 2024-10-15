5ab53155303be6b6b009ea944c07876127abbb73
Joonas Montonen <159522623+jaycm18@users.noreply.github.com>
2024-10-15 10:12:29 +0300

    Lisätty Employees ja Products controllerit

---
+++ Controllers/EmployeesController.cs
@@ -0,0 +1,121 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NorthwindRestApi.Models;
+
+namespace NorthwindRestApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeesController : ControllerBase
+    {
+        private readonly NorthwindContext db;
+
+        public EmployeesController(NorthwindContext dbparametri)
+        {
+            db = dbparametri;
+        }
+
+        // Hakee kaikki työntekijät
+        [HttpGet]
+        public IActionResult GetAllEmployees()
+        {
+            try
+            {
+                var tyontekijat = db.Employees.ToList();
+                return Ok(tyontekijat);
+            }
+            catch (Exception e)
+            {
+                return BadRequest("Tapahtui virhe. Lue lisää: " + e.InnerException);
+            }
+        }
+
+        // Hakee työntekijän id:n perusteella
+        [HttpGet("{id}")]
+        public ActionResult GetOneEmployeeById(int id)
+        {
+            try
+            {
+                var tyontekija = db.Employees.Find(id);
+                if (tyontekija != null)
+                {
+                    return Ok(tyontekija);
+                }
+                else
+                {
+                    return NotFound($"Työntekijää id:llä {id} ei löydy.");
+                }
+            }
+            catch (Exception e)
+            {
+                return BadRequest("Tapahtui virhe. Lue lisää: " + e);
+            }
+        }
+
+        // Uuden työntekijän lisääminen
+        [HttpPost]
+        public ActionResult AddNew([FromBody] Employee employee)
+        {
+            try
+            {
+                db.Employees.Add(employee);
+                db.SaveChanges();
+                return Ok($"Lisättiin uusi työntekijä {employee.FirstName} {employee.LastName}");
+            }
+            catch (Exception e)
+            {
+                return BadRequest("Tapahtui virhe. Lue lisää: " + e.InnerException);
+            }
+        }
+
+        // Työntekijän poistaminen
+        [HttpDelete("{id}")]
+        public ActionResult Delete(int id)
+        {
+            try
+            {
+                var tyontekija = db.Employees.Find(id);
+                if (tyontekija != null)
+                {
+                    db.Employees.Remove(tyontekija);
+                    db.SaveChanges();
+                    return Ok($"Työntekijä {tyontekija.FirstName} {tyontekija.LastName} poistettiin.");
+                }
+                return NotFound($"Työntekijää id:llä {id} ei löytynyt.");
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.InnerException);
+            }
+        }
+
+        // Työntekijän muokkaaminen
+        [HttpPut("{id}")]
+        public ActionResult EditEmployee(int id, [FromBody] Employee employee)
+        {
+            var tyontekija = db.Employees.Find(id);
+            if (tyontekija != null)
+            {
+                tyontekija = employee;
+                db.SaveChanges();
+                return Ok($"Muokattu työntekijää {tyontekija.FirstName} {tyontekija.LastName}");
+            }
+            return NotFound($"Työntekijää ei löytynyt id:llä {id}");
+        }
+
+        // Hakee työntekijän nimen osalla: /api/employees/name/hakusana
+        [HttpGet("name/{ename}")]
+        public ActionResult GetByName(string ename)
+        {
+            try
+            {
+                var tyontekijat = db.Employees.Where(e => e.FirstName.Contains(ename) || e.LastName.Contains(ename));
+                return Ok(tyontekijat);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,121 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NorthwindRestApi.Models;
+
+namespace NorthwindRestApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly NorthwindContext db;
+
+        public ProductsController(NorthwindContext dbparametri)
+        {
+            db = dbparametri;
+        }
+
+        // Hakee kaikki tuotteet
+        [HttpGet]
+        public IActionResult GetAllProducts()
+        {
+            try
+            {
+                var tuotteet = db.Products.ToList();
+                return Ok(tuotteet);
+            }
+            catch (Exception e)
+            {
+                return BadRequest("Tapahtui virhe. Lue lisää: " + e.InnerException);
+            }
+        }
+
+        // Hakee tuotteen id:n perusteella
+        [HttpGet("{id}")]
+        public ActionResult GetOneProductById(int id)
+        {
+            try
+            {
+                var tuote = db.Products.Find(id);
+                if (tuote != null)
+                {
+                    return Ok(tuote);
+                }
+                else
+                {
+                    return NotFound($"Tuotetta id:llä {id} ei löydy.");
+                }
+            }
+            catch (Exception e)
+            {
+                return BadRequest("Tapahtui virhe. Lue lisää: " + e);
+            }
+        }
+
+        // Uuden tuotteen lisääminen
+        [HttpPost]
+        public ActionResult AddNew([FromBody] Product product)
+        {
+            try
+            {
+                db.Products.Add(product);
+                db.SaveChanges();
+                return Ok($"Lisättiin uusi tuote {product.ProductName}");
+            }
+            catch (Exception e)
+            {
+                return BadRequest("Tapahtui virhe. Lue lisää: " + e.InnerException);
+            }
+        }
+
+        // Tuotteen poistaminen
+        [HttpDelete("{id}")]
+        public ActionResult Delete(int id)
+        {
+            try
+            {
+                var tuote = db.Products.Find(id);
+                if (tuote != null)
+                {
+                    db.Products.Remove(tuote);
+                    db.SaveChanges();
+                    return Ok($"Tuote {tuote.ProductName} poistettiin.");
+                }
+                return NotFound($"Tuotetta id:llä {id} ei löytynyt.");
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.InnerException);
+            }
+        }
+
+        // Tuotteen muokkaaminen
+        [HttpPut("{id}")]
+        public ActionResult EditProduct(int id, [FromBody] Product product)
+        {
+            var tuote = db.Products.Find(id);
+            if (tuote != null)
+            {
+                tuote = product;
+                db.SaveChanges();
+                return Ok($"Muokattu tuotetta {tuote.ProductName}");
+            }
+            return NotFound($"Tuotetta ei löytynyt id:llä {id}");
+        }
+
+        // Hakee tuotteen nimen osalla: /api/products/name/hakusana
+        [HttpGet("name/{pname}")]
+        public ActionResult GetByName(string pname)
+        {
+            try
+            {
+                var tuotteet = db.Products.Where(p => p.ProductName.Contains(pname));
+                return Ok(tuotteet);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
