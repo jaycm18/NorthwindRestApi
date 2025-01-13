758a773eb3b7da338d7805188d23c7018f5ae72e
Joonas Montonen <159522623+jaycm18@users.noreply.github.com>
2025-01-13 12:13:49 +0200

    Cypress  testit päivitys ja kommentit

--- cypress/e2e/authentication.spec.cy.js
+++ cypress/e2e/authentication.spec.cy.js
@@ -2,25 +2,25 @@ describe('Authentication Test', () => {
     it('should login successfully with valid credentials', () => {
         cy.visit('http://localhost:5173'); // K‰yt‰ oikeaa URL-osoitetta
 
-        cy.get('input[placeholder="Username"]', { timeout: 10000 }).type('teppo'); // Lis‰‰ timeout
-        cy.get('input[placeholder="Password"]', { timeout: 10000 }).type('testaaja'); // Lis‰‰ timeout
+        cy.get('input[placeholder="Username"]', { timeout: 10000 }).type('teppo'); 
+        cy.get('input[placeholder="Password"]', { timeout: 10000 }).type('testaaja'); 
         cy.get('input[type="submit"]', { timeout: 10000 }).click();
 
         // Tarkistetaan, ett‰ tervetuloviesti n‰kyy kirjautumisen j‰lkeen
-        cy.contains('Logged in as: teppo', { timeout: 10000 }).should('be.visible'); // Korvaa tervetuloviestill‰, joka n‰kyy kirjautumisen j‰lkeen
+        cy.contains('Logged in as: teppo', { timeout: 10000 }).should('be.visible'); // Tervetuloviesti
     });
 
     it('should navigate to customers page and display customer list after login', () => {
         cy.visit('http://localhost:5173'); // K‰yt‰ oikeaa URL-osoitetta
 
-        cy.get('input[placeholder="Username"]', { timeout: 10000 }).type('teppo'); // Lis‰‰ timeout
-        cy.get('input[placeholder="Password"]', { timeout: 10000 }).type('testaaja'); // Lis‰‰ timeout
+        cy.get('input[placeholder="Username"]', { timeout: 10000 }).type('teppo'); 
+        cy.get('input[placeholder="Password"]', { timeout: 10000 }).type('testaaja'); 
         cy.get('input[type="submit"]', { timeout: 10000 }).click();
 
         // Klikataan Customers-v‰lilehte‰ kirjautumisen j‰lkeen
-        cy.contains('Customers', { timeout: 10000 }).click(); // Korvaa 'Customers' oikealla v‰lilehden nimell‰
+        cy.contains('Customers', { timeout: 10000 }).click(); 
 
         // Klikataan Customers-nappia, joka n‰ytt‰‰ customerlistin
-        cy.get('h1 nobr', { timeout: 10000 }).click(); // Korvaa 'h1 nobr' oikealla valitsimella, joka vastaa Customers-nappia
+        cy.get('h1 nobr', { timeout: 10000 }).click();
     });
 });
